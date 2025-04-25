6479c3c58547ddd044b93f634bed336a191ac1f8
DomeroSoftware <domerosoftware@gmail.com>
2025-04-25 23:43:47 +0200

    Add files via upload

--- example/gerr.pl
+++ example/gerr.pl
@@ -3,12 +3,11 @@
 use strict;
 use warnings;
 use utf8; # Enable UTF-8 support
-use lib '../lib'; # Adjust the path as needed to locate gerr.pm
-use gerr;
+use gerr qw(error trace);
 
 # Demonstration of the error function
 eval {
-    error("This is a fatal error message", "type=Example Error", "trace=1", "return=1");
+    print STDERR error("This is a fatal error message", "type=Example Error", "trace=1", "return=1");
 };
 
 if ($@) {
@@ -19,7 +18,7 @@ if ($@) {
 # Demonstration of the trace function
 sub example_subroutine {
     my $stack_trace = trace(3);
-    print "Stack trace:\n$stack_trace";
+    print STDERR "Stack trace:\n$stack_trace";
 }
 
 example_subroutine();
